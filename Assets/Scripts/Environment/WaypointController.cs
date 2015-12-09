fc993ac81d55670e829fe8a936e466bf16e524df
conovich <conovich@gmail.com>
2015-12-09 17:20:45 -0500

    Set up waypoints.

---
+++ Assets/Scripts/Environment/WaypointController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointController : MonoBehaviour {
+	GameObject[] waypoints;
+
+	void Awake(){
+		GetWaypoints ();
+	}
+
+	void GetWaypoints(){
+		waypoints = GameObject.FindGameObjectsWithTag ("Waypoint");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
