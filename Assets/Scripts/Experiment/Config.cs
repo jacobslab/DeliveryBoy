121510ea4cce52aa5af44818073ac1a8d10fef1b
conovich <conovich@gmail.com>
2016-02-15 15:12:21 -0500

    Changed num delivery stores back to 13.

--- Assets/Scripts/Experiment/Config.cs
+++ Assets/Scripts/Experiment/Config.cs
@@ -44,7 +44,7 @@ public class Config : MonoBehaviour {
 	public static float randomJitterMax = 0.2f;
 
 	//DELIVERY VARIABLES
-	public static int numDeliveryStores = 4; //out of the total number of stores -- LAST STORE DOES NOT ACTUALLY GET DELIVERY
+	public static int numDeliveryStores = 13; //out of the total number of stores -- LAST STORE DOES NOT ACTUALLY GET DELIVERY
 	
 	public static bool isAudioDelivery = true;
 
