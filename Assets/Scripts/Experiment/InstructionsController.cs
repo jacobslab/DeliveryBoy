56f554e99227925d0c49e1502e6b14f6cff208d7
conovich <conovich@gmail.com>
2016-04-28 18:17:57 -0400

    Added German instructions.

--- Assets/Scripts/Experiment/InstructionsController.cs
+++ Assets/Scripts/Experiment/InstructionsController.cs
@@ -27,13 +27,14 @@ public class InstructionsController : MonoBehaviour {
 
 
 	//INITIAL INSTRUCTIONS
+#if (!GERMAN)
 	public static string initialInstructions1 = "INTRODUCTION" +
 		"\n\nIn this game you will play a delivery person in a small city. " +
 		"Your task is to drive through the city delivering packages to the correct stores, as quickly as possible. " +
 		"\n\nYour current delivery goal will be shown in the top left of the screen.  Simply drive right up to the store, and the item you delivered will spoken out loud. " +
-		"On each trial you will make a series of deliveries to stores all over the town. ";
-	
-	public static string initialInstructions2 = "\n\nOn the final delivery of a given trial, no item will be said. " +
+		"On each trial you will make a series of deliveries to stores all over the town. " +
+
+		"\n\nOn the final delivery of a given trial, no item will be said. " +
 		"Rather, the screen will go blank, and you'll see a row of asterisks (*******), and hear a tone. " +
 		"When the tone has finished and the asterisks turn red, your job is to verbally recall all of the delivered items that you can remember, in any order. " +
 		"This free recall period will last for " + Config.freeRecallTime + " seconds. " +
@@ -43,7 +44,7 @@ public class InstructionsController : MonoBehaviour {
 
 			"\n\nAfter this cued recall period, you'll have a chance for a short break, and then the next set of deliveries will start. ";
 
-	public static string initialInstructions3Learning = "\n\nBefore you start the full task, you'll have a chance to explore the town, to get your bearings." +
+	public static string initialInstructions2Learning = "\n\nBefore you start the full task, you'll have a chance to explore the town, to get your bearings." +
 		"We will describe this practice period next." +
 			"\n\nPlease tell the investigator when you have finished reading these instructions.";
 	
@@ -53,12 +54,12 @@ public class InstructionsController : MonoBehaviour {
 		"\n\nBefore you start making deliveries, we want to make sure you know your way around the city. " +
 		" You'll be asked to go from store to store without delivering items. " +
 		"The city will be exactly the same during the practice as during the later delivery trials, so you can use this time to figure out the fastest way to get from place to place. " +
-			"To help you out, we will often send you from one store to another store that is nearby in the town.";
-	
-	public static string learningInstructions2 = "\n\nDon't worry if it takes a little while to learn the city!" +
+			"To help you out, we will often send you from one store to another store that is nearby in the town." +
+
+		"\n\nDon't worry if it takes a little while to learn the city!" +
 		/*" Your bonus is not affected by how long the practice period takes." +*/
-			"\n\nFinding the correct stores will be difficult at first, but it will get easier when you become more familiar with the city." +
-			"\n\nPlease tell the investigator when you have finished reading.";
+		"\n\nFinding the correct stores will be difficult at first, but it will get easier when you become more familiar with the city." +
+		"\n\nPlease tell the investigator when you have finished reading.";
 
 
 	public static string finalItemRecallInstructions = "In this next period, please recall as many ITEMS as you can remember from the entire session, in any order. " +
@@ -68,7 +69,57 @@ public class InstructionsController : MonoBehaviour {
 	public static string finalStoreRecallInstructions = "In this next period, please recall as many STORE NAMES as you can remember from the entire session, in any order. " +
 		"You will be given several minutes to do this; please keep trying for the whole period, as you may find that words keep springing up in your memory." +
 		"\n\n* Press (X) to begin the recall period *";
+#elif GERMAN
+	public static string initialInstructions1 = "ANLEITUNG" +
+		"\n\nIn diesem Computerspiel übernehmen Sie die Aufgabe eines Kurierboten in einer kleinen Stadt. " +
+		"Ihre Aufgabe ist es durch die Stadt zu fahren und, so schnell wie möglich, Pakete an die richtigen Geschäfte zu liefern. " +
 
+		"\n\nDas Ziel jeder Lieferung wird oben links auf dem Bildschirm angezeigt. Wenn Sie am Ziel angekommen sind fahren Sie einfach direkt " +
+		"an das Geschäft heran und der Gegenstand den Sie geliefert haben wird angezeigt. Während des Spiels haben Sie die Gelegenheit eine Reihe " +
+		"solcher Lieferungen in der ganzen Stadt zu tätigen." +
+
+		"\n\nAm Ende der letzten Lieferung einer solchen Serie werden Sie an Stelle eines Gegenstandes einen leeren Bildschirm mit einer Reihe von " +
+		"Sternchen (*******) sehen und einen Ton hören. Dies ist Ihr Signal um so viele der gelieferten Gegenstände wie möglich laut aufzuzählen (die " +
+		"Reihenfolge ist dabei unwichtig)." +
+
+	//TODO: TRANSLATE TO GERMAN
+		"When the tone has finished and the asterisks turn red, your job is to verbally recall all of the delivered items that you can remember, in any order. " +
+		"This free recall period will last for " + Config.freeRecallTime + " seconds. " +
+		"Once this free recall period has ended, you will either be shown a building and asked to recall the item you delivered, " +
+		"or you will hear an item that you delivered, and will be asked to recall the store to which you delivered it. " +
+		"You will have 6 seconds to recall the matching item or store." +
+
+		"Nach diesem Gedächtnis Test können Sie eine kurze Pause machen bevor eine neue Runde von Lieferungen beginnt.";
+	
+	public static string initialInstructions2Learning = "\n\nBevor das eigentliche Spiel beginnt, lassen wir Sie erstmal die Stadt " +
+		"erkunden damit Sie sich orientieren können. Diese Übungsphase werden wir Ihnen als nächstes erklären." +
+		"\n\nBitte sagen Sie dem Versuchsleiter Bescheid wenn sie diese Anleitung zu Ende gelesen haben.";
+	
+	
+	//LEARNING PHASE INSTRUCTIONS
+	public static string learningInstructions = "ÜBUNGSPHASE" +
+		"\n\nBevor Sie mit den Lieferungen beginnen, möchten wir Sie mit der Stadt vertraut machen. Wir werden Sie bitten zu verschiedenen Geschäften " +
+		"zu fahren ohne Lieferungen vorzunehmen. Sie werden später in der gleichen Stadt Lieferungen vornehmen, daher ist es günstig wenn Sie " + 
+		"diese Übungsphase nutzen um die kürzesten Wege zwischen Geschäften herauszufinden. Als kleine Hilfe werde wir Sie in dieser Übungsphase " +
+		"oft bitten zu naheliegenden Geschäften zu fahren." +
+	
+		"\n\nMachen Sie sich keine Sorgen, wenn Sie etwas länger brauchen um sich mit der Stadt vertraut zu machen. " +
+		"\n\nZunächst wird es schwierig sein die richtigen Geschäfte zu finden, aber mit der Zeit sollten Sie ein gutes Gefühl für die Stadt bekommen." +
+		"\n\nBitte sagen Sie dem Versuchsleiter bescheid wenn Sie diese Anleitung zu Ende gelesen haben.";
+	
+	
+	public static string finalItemRecallInstructions = "In dem folgenden Gedächtnis Test, zählen Sie bitte so viele von allen " +
+		"heute gelieferten GEGENSTÄNDEN (d.h. von allen Lieferphasen) laut auf wie möglich (die Reihenfolge ist dabei unwichtig). Sie haben dafür " +
+		"mehrere Minuten Zeit. Bitte versuchen Sie sich bis zum Ende diese Zeitraumes an die Namen von Geschäften zu erinnern, da Ihnen u.U. auch " + 
+		"noch Gegenstände einfallen, wenn Sie glauben sich an keine weiteren erinnern zu können." +
+			"\n\n* Bitte drücken Sie (X) wenn Sie bereit sind  *";
+	
+	public static string finalStoreRecallInstructions = "Im folgenden Gedächtnis Test zählen Sie bitte so viele NAMEN VON" +
+		"GESCHÄFTEN laut auf wie möglich (die Reihenfolge ist dabei unwichtig). Sie haben dafür mehrere Minuten Zeit. Bitte versuchen Sie "+
+		"sich bis zum Ende dieses Zeitraumes an die Namen von Geschäften zu erinnern, da Ihnen u.U. auch noch Namen einfallen, wenn Sie glauben " +
+		"sich an keine weiteren erinnern zu können." +
+		"\n\n* Bitte drücken Sie (X) wenn Sie bereit sind *";
+#endif
 
 	//ROTATION PHASE INSTRUCTIONS
 	public static string rotationInstructions1 = "Now you will see images of the stores in the city so that you can better recognize them while doing the task. " +
@@ -87,7 +138,7 @@ public class InstructionsController : MonoBehaviour {
 	}
 
 	public IEnumerator PlayStartInstructions(){
-		yield return StartCoroutine (ShowSingleInstruction (InstructionsController.initialInstructions1 + InstructionsController.initialInstructions2 + InstructionsController.initialInstructions3Learning, true, true, false, Config.minInitialInstructionsTime));
+		yield return StartCoroutine (ShowSingleInstruction (InstructionsController.initialInstructions1 + InstructionsController.initialInstructions2Learning, true, true, false, Config.minInitialInstructionsTime));
 		/*yield return StartCoroutine (ShowSingleInstruction (InstructionsController.initialInstructions2, true, true, false, Config.minInitialInstructionsTime));
 		if (Config.doLearningPhase) {
 			yield return StartCoroutine (ShowSingleInstruction (InstructionsController.initialInstructions3Learning, true, true, false, Config.minInitialInstructionsTime));
@@ -99,7 +150,7 @@ public class InstructionsController : MonoBehaviour {
 	}
 
 	public IEnumerator PlayLearningInstructions(){
-		yield return StartCoroutine (ShowSingleInstruction (InstructionsController.learningInstructions1 + InstructionsController.learningInstructions2, true, true, false, Config.minInitialInstructionsTime));
+		yield return StartCoroutine (ShowSingleInstruction (InstructionsController.learningInstructions, true, true, false, Config.minInitialInstructionsTime));
 		//yield return StartCoroutine (ShowSingleInstruction (InstructionsController.learningInstructions2, true, true, false, Config.minInitialInstructionsTime));
 	}
 
