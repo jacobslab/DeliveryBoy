49b9ca1c031136bd564c3b3a5b3b3d9c3e55dea0
Ansh <lightnarcissus@gmail.com>
2016-09-28 16:31:18 -0400

    Updated to 5.4 and imported SMI eyetracker package and implemented a gaze follower in the MainGame scene

---
+++ Assets/MyTests/GazeFollower2D.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using iView;
+
+public class GazeFollower2D : MonoBehaviour {
+
+	public Canvas myCanvas;
+	Transform gazeFollower;
+
+	void Awake(){
+		gazeFollower = GetComponent<Transform> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gazeFollower != null) {
+			Vector2 screenGazePos = SMIGazeController.Instance.GetSample ().averagedEye.gazePosInUnityScreenCoords ();
+			Debug.Log("SCREEN POS: " + screenGazePos);
+			Vector3 worldGazePos = Camera.main.ScreenToWorldPoint(new Vector3 ( screenGazePos.x, screenGazePos.y, gazeFollower.position.z));
+			Debug.Log("WORLD POS: " + worldGazePos);
+			//gazeFollower.position = new Vector3(screenGazePos.x, screenGazePos.y, gazeFollower.position.z);
+			//gazeFollower.position = worldGazePos;
+
+
+			Vector2 pos;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, screenGazePos, myCanvas.worldCamera, out pos);
+			gazeFollower.position = myCanvas.transform.TransformPoint(pos);
+
+		}
+	}
+}
