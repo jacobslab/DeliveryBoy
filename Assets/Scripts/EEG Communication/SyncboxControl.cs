c7426be2215dad20e3a8af69950a95fd7b13aa35
Ansh Patel <lightnarcissus@gmail.com>
2017-03-08 17:57:15 -0500

    Fixed  UI resolution aspect ratio to scale properly for task laptop

--- Assets/Scripts/EEG Communication/SyncboxControl.cs
+++ Assets/Scripts/EEG Communication/SyncboxControl.cs
@@ -4,10 +4,10 @@ using System;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 
-public class SyncboxControl : MonoBehaviour {
-	Experiment exp { get { return Experiment.Instance; } }
+	public class SyncboxControl : MonoBehaviour {
+		Experiment exp { get { return Experiment.Instance; } }
 
-	#if GERMAN
+#if GERMAN
 	[DllImport ("FreiburgSyncboxPlugin")]
 	private static extern IntPtr OpenUSB();
 	[DllImport ("FreiburgSyncboxPlugin")]
@@ -16,182 +16,184 @@ public class SyncboxControl : MonoBehaviour {
 	private static extern IntPtr TurnLEDOn();
 	[DllImport ("FreiburgSyncboxPlugin")]
 	private static extern IntPtr TurnLEDOff();
+#else
+		[DllImport ("ASimplePlugin")]
+		private static extern IntPtr OpenUSB();
+		[DllImport ("ASimplePlugin")]
+		private static extern IntPtr CloseUSB();
+		[DllImport ("ASimplePlugin")]
+		private static extern IntPtr TurnLEDOn();
+		[DllImport ("ASimplePlugin")]
+		private static extern IntPtr TurnLEDOff();
+		[DllImport ("ASimplePlugin")]
+		private static extern long SyncPulse();
+		[DllImport ("ASimplePlugin")]
+		private static extern IntPtr StimPulse(float durationSeconds, float freqHz, bool doRelay);
+#endif
+		public bool ShouldSyncPulse = true;
+		public float PulseOnSeconds;
+		public float PulseOffSeconds;
 
-	#else
-	[DllImport ("ASimplePlugin")]
-	private static extern IntPtr OpenUSB();
-	[DllImport ("ASimplePlugin")]
-	private static extern IntPtr CloseUSB();
-	[DllImport ("ASimplePlugin")]
-	private static extern IntPtr TurnLEDOn();
-	[DllImport ("ASimplePlugin")]
-	private static extern IntPtr TurnLEDOff();
-	//[DllImport ("ASimplePlugin")]
-	//private static extern long SyncPulse();
-	//[DllImport ("ASimplePlugin")]
-	//private static extern IntPtr StimPulse(float durationSeconds, float freqHz, bool doRelay);
-	#endif
-
-	public bool ShouldSyncPulse = true;
-	public float PulseOnSeconds;
-	public float PulseOffSeconds;
-
-	public bool isUSBOpen = false;
+		public bool isUSBOpen = false;
 
 
-	//SINGLETON
-	private static SyncboxControl _instance;
+		//SINGLETON
+		private static SyncboxControl _instance;
 
-	public static SyncboxControl Instance{
-		get{
-			return _instance;
-		}
-	}
-
-	void Awake(){
-
-		if (_instance != null) {
-			UnityEngine.Debug.Log("Instance already exists!");
-			Destroy(transform.gameObject);
-			return;
-		}
-		_instance = this;
-
-	}
-
-	// Use this for initialization
-	void Start () {
-		if(Config.isSyncbox){
-			StartCoroutine(ConnectSyncbox());
-		}
-	}
-
-	IEnumerator ConnectSyncbox(){
-		while(!isUSBOpen){
-			string usbOpenFeedback = Marshal.PtrToStringAuto (OpenUSB());
-			UnityEngine.Debug.Log(usbOpenFeedback);
-			if(usbOpenFeedback != "didn't open USB..."){
-				isUSBOpen = true;
+		public static SyncboxControl Instance{
+			get{
+				return _instance;
 			}
-
-			yield return 0;
 		}
 
-		StartCoroutine (RunSyncPulseManual ());
-	}
+		void Awake(){
 
-	// Update is called once per frame
-	void Update () {
-		GetInput ();
-	}
+			if (_instance != null) {
+				UnityEngine.Debug.Log("Instance already exists!");
+				Destroy(transform.gameObject);
+				return;
+			}
+			_instance = this;
 
-	void GetInput(){
-		//use this for debugging if you'd like
-	}
+		}
 
-	float syncPulseDuration = 0.05f;
-	float syncPulseInterval = 1.0f;
-	/*	IEnumerator RunSyncPulse(){
-		Stopwatch executionStopwatch = new Stopwatch ();
-		while (ShouldSyncPulse) {
-			executionStopwatch.Reset();
-			SyncPulse(); //executes pulse, then waits for the rest of the 1 second interval
-			executionStopwatch.Start();
-			long syncPulseOnTime = SyncPulse();
-			LogSYNCOn(syncPulseOnTime);
-			while(executionStopwatch.ElapsedMilliseconds < 1500){
+		// Use this for initialization
+		void Start () {
+			if(Config.isSyncbox){
+				StartCoroutine(ConnectSyncbox());
+			}
+		}
+
+		IEnumerator ConnectSyncbox(){
+			while(!isUSBOpen){
+				string usbOpenFeedback = Marshal.PtrToStringAuto (OpenUSB());
+				UnityEngine.Debug.Log(usbOpenFeedback);
+				if(usbOpenFeedback != "didn't open USB..."){
+					isUSBOpen = true;
+				}
+
 				yield return 0;
 			}
-			executionStopwatch.Stop();
+
+			StartCoroutine (RunSyncPulseManual ());
 		}
-	}*/
 
-	IEnumerator RunSyncPulseManual(){
-		float jitterMin = 0.1f;
-		float jitterMax = syncPulseInterval - syncPulseDuration;
+		// Update is called once per frame
+		void Update () {
+			GetInput ();
+		}
 
-		Stopwatch executionStopwatch = new Stopwatch ();
+		void GetInput(){
+			//use this for debugging if you'd like
+		}
 
-		while (ShouldSyncPulse) {
-			executionStopwatch.Reset();
+		float syncPulseDuration = 0.05f;
+		float syncPulseInterval = 1.0f;
+	/*
+		IEnumerator RunSyncPulse(){
+			Stopwatch executionStopwatch = new Stopwatch ();
+
+			while (ShouldSyncPulse) {
+				executionStopwatch.Reset();
+
+				SyncPulse(); //executes pulse, then waits for the rest of the 1 second interval
+
+				executionStopwatch.Start();
+				long syncPulseOnTime = SyncPulse();
+				LogSYNCOn(syncPulseOnTime);
+				while(executionStopwatch.ElapsedMilliseconds < 1500){
+					yield return 0;
+				}
+
+				executionStopwatch.Stop();
+
+			}
+		}
+*/
+
+		//WE'RE USING THIS FUNCTION
+		IEnumerator RunSyncPulseManual(){
+			float jitterMin = 0.1f;
+			float jitterMax = syncPulseInterval - syncPulseDuration;
+
+			Stopwatch executionStopwatch = new Stopwatch ();
+
+			while (ShouldSyncPulse) {
+				executionStopwatch.Reset();
 
 
-			float jitter = UnityEngine.Random.Range(jitterMin, jitterMax);//syncPulseInterval - syncPulseDuration);
-			yield return StartCoroutine(WaitForShortTime(jitter));
+				float jitter = UnityEngine.Random.Range(jitterMin, jitterMax);//syncPulseInterval - syncPulseDuration);
+				yield return StartCoroutine(WaitForShortTime(jitter));
 
-			ToggleLEDOn();
-			yield return StartCoroutine(WaitForShortTime(syncPulseDuration));
-			ToggleLEDOff();
+				ToggleLEDOn();
+				yield return StartCoroutine(WaitForShortTime(syncPulseDuration));
+				ToggleLEDOff();
 
-			float timeToWait = (syncPulseInterval - syncPulseDuration) - jitter;
-			if(timeToWait < 0){
-				timeToWait = 0;
+				float timeToWait = (syncPulseInterval - syncPulseDuration) - jitter;
+				if(timeToWait < 0){
+					timeToWait = 0;
+				}
+
+				yield return StartCoroutine(WaitForShortTime(timeToWait));
+
+				executionStopwatch.Stop();
+			}
+		}
+
+		//return microseconds it took to turn on LED
+		void ToggleLEDOn(){
+
+			TurnLEDOn ();
+			LogSYNCOn (GameClock.SystemTime_Milliseconds);
+		}
+
+		void ToggleLEDOff(){
+
+			TurnLEDOff();
+			LogSYNCOff (GameClock.SystemTime_Milliseconds);
+
+		}
+
+		long GetMicroseconds(long ticks){
+			long microseconds = ticks / (TimeSpan.TicksPerMillisecond / 1000);
+			return microseconds;
+		}
+
+		IEnumerator WaitForShortTime(float jitter){
+			float currentTime = 0.0f;
+			while (currentTime < jitter) {
+				currentTime += Time.deltaTime;
+				yield return 0;
 			}
 
-			yield return StartCoroutine(WaitForShortTime(timeToWait));
-
-			executionStopwatch.Stop();
 		}
-	}
 
-	//return microseconds it took to turn on LED
-	void ToggleLEDOn(){
+		void LogSYNCOn(long time){
+			if (ExperimentSettings.isLogging) {
+				exp.eegLog.Log (time, exp.eegLog.GetFrameCount(), "ON"); //NOTE: NOT USING FRAME IN THE FRAME SLOT
+			}
+		}
 
-		TurnLEDOn ();
-		LogSYNCOn (GameClock.SystemTime_Milliseconds);
-	}
+		void LogSYNCOff(long time){
+			if (ExperimentSettings.isLogging) {
+				exp.eegLog.Log (time, exp.eegLog.GetFrameCount(), "OFF"); //NOTE: NOT USING FRAME IN THE FRAME SLOT
+			}
+		}
 
-	void ToggleLEDOff(){
+		void LogSYNCStarted(long time, float duration){
+			if (ExperimentSettings.isLogging) {
+				exp.eegLog.Log (time, exp.eegLog.GetFrameCount (), "SYNC PULSE STARTED" + Logger_Threading.LogTextSeparator + duration);
+			}
+		}
 
-		TurnLEDOff();
-		LogSYNCOff (GameClock.SystemTime_Milliseconds);
+		void LogSYNCPulseInfo(long time, float timeBeforePulseSeconds){
+			if (ExperimentSettings.isLogging) {
+				exp.eegLog.Log (time, exp.eegLog.GetFrameCount (), "SYNC PULSE INFO" + Logger_Threading.LogTextSeparator + timeBeforePulseSeconds*1000); //log milliseconds
+			}
+		}
 
-	}
-
-	long GetMicroseconds(long ticks){
-		long microseconds = ticks / (TimeSpan.TicksPerMillisecond / 1000);
-		return microseconds;
-	}
-
-	IEnumerator WaitForShortTime(float jitter){
-		float currentTime = 0.0f;
-		while (currentTime < jitter) {
-			currentTime += Time.deltaTime;
-			yield return 0;
+		void OnApplicationQuit(){
+			UnityEngine.Debug.Log(Marshal.PtrToStringAuto (CloseUSB()));
 		}
 
 	}
-
-	void LogSYNCOn(long time){
-		if (ExperimentSettings.isLogging) {
-			exp.eegLog.Log (time, exp.eegLog.GetFrameCount(), "ON"); //NOTE: NOT USING FRAME IN THE FRAME SLOT
-		}
-	}
-
-	void LogSYNCOff(long time){
-		if (ExperimentSettings.isLogging) {
-			exp.eegLog.Log (time, exp.eegLog.GetFrameCount(), "OFF"); //NOTE: NOT USING FRAME IN THE FRAME SLOT
-		}
-	}
-
-	void LogSYNCStarted(long time, float duration){
-		if (ExperimentSettings.isLogging) {
-			exp.eegLog.Log (time, exp.eegLog.GetFrameCount (), "SYNC PULSE STARTED" + Logger_Threading.LogTextSeparator + duration);
-		}
-	}
-
-	void LogSYNCPulseInfo(long time, float timeBeforePulseSeconds){
-		if (ExperimentSettings.isLogging) {
-			exp.eegLog.Log (time, exp.eegLog.GetFrameCount (), "SYNC PULSE INFO" + Logger_Threading.LogTextSeparator + timeBeforePulseSeconds*1000); //log milliseconds
-		}
-	}
-
-	void OnDestroy(){
-		UnityEngine.Debug.Log(Marshal.PtrToStringAuto (CloseUSB()));
-	}
-
-	void OnApplicationQuit(){
-		UnityEngine.Debug.Log(Marshal.PtrToStringAuto (CloseUSB()));
-	}
-
-}
