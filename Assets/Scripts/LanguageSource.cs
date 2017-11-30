26feb239c23750d5e389a3883784a093b30a14b8
Henry Jonas Solberg <milkshakeiii@gmail.com>
2017-11-30 16:23:43 -0500

    menu work

---
+++ Assets/Scripts/LanguageSource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LanguageSource
+{
+    private enum LANGUAGE { ENGLISH, GERMAN };
+    private LANGUAGE current_language;
+
+
+}
